98dc8cb85741cccf34762c4d9622f8f55ebbf768
RoSchmi <Dr.Roland.Schmidt@t-online.de>
2020-06-08 19:08:20 +0200

    Deletions

--- mqtt-ana-struct-2/src/Pods/Target Support Files/Pods-MQTTAnalyzer/Pods-MQTTAnalyzer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MQTTAnalyzer : NSObject
-@end
-@implementation PodsDummy_Pods_MQTTAnalyzer
-@end
--- mqtt-ana-struct-2/src/Pods/Target Support Files/swift-petitparser/swift-petitparser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_swift_petitparser : NSObject
-@end
-@implementation PodsDummy_swift_petitparser
-@end
